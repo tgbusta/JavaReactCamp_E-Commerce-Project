0ab6ad6fcec14b809a2f7e25a6957e80d984e935
tgbusta <69412064+tgbusta@users.noreply.github.com>
2021-05-22 09:54:32 +0300

    Add files via upload

--- src/Lesson5/Main.java
+++ src/Lesson5/Main.java
@@ -1,10 +1,18 @@
 package Lesson5;
 
+import Lesson5.business.concretes.UserValidateManager;
+import Lesson5.core.concretes.SignInWithGoogle;
+import Lesson5.entities.concretes.User;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+			User user=new User(1, "Tuðba", "Usta", "tbozkurtusta@gmail.com", "tgbst", "123");
+			UserValidateManager userValidateManager= new UserValidateManager();
+			userValidateManager.passwordValidation(user.getPassword());
+			userValidateManager.emailValidation(user.getEmail());
+			SignInWithGoogle signInWithGoogle= new SignInWithGoogle();
+			signInWithGoogle.onSuccess("googleSingInClientId", user.getUserName(), user.getPassword());
 	}
 
 }
